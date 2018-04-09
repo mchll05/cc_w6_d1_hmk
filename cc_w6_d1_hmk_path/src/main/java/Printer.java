8e468cc8d5e7d857b11e6b25e6ab52c34419c174
Michelle Austin <mchllaustin@gmail.com>
2018-04-09 22:20:21 +0100

    printer finished

--- cc_w6_d1_hmk_path/src/main/java/Printer.java
+++ cc_w6_d1_hmk_path/src/main/java/Printer.java
@@ -1,2 +1,31 @@
 public class Printer {
+
+    private int paper;
+    private int toner;
+
+    public Printer(int paper, int toner) {
+
+        this.paper = paper;
+        this.toner = toner;
+}
+
+    public int getNumber() {
+        return this.paper;
+    }
+
+    public int printPaper() {
+        return this.paper -= 1;
+    }
+
+    public int refillPrinter() {
+        return this.paper += 50;
+    }
+
+    public int getToner() {
+        return this.toner;
+    }
+
+    public int tonerDecrease() {
+        return this.toner -= 1;
+    }
 }
--- cc_w6_d1_hmk_path/src/test/java/PrinterTest.java
+++ cc_w6_d1_hmk_path/src/test/java/PrinterTest.java
@@ -8,13 +8,34 @@ public class PrinterTest {
 
     @Before
     public void before() {
-        myPrinter = new Printer();
-
+        myPrinter = new Printer(10, 100);
     }
 
     @Test
-    public void hasName() {
-        assertEquals("Paddington", myBear.getName());
+    public void howMuchPaper() {
+        assertEquals(10, myPrinter.getNumber());
+    }
 
+    @Test
+    public void printPaperNumber() {
+        myPrinter.printPaper();
+        assertEquals(9, myPrinter.getNumber());
+    }
+    @Test
+    public void refillPrinterPaper() {
+        myPrinter.refillPrinter();
+        assertEquals(60, myPrinter.getNumber());
+    }
+
+    @Test
+    public void howMuchToner() {
+        assertEquals(100, myPrinter.getToner());
+    }
+
+    @Test
+    public void decreaseToner() {
+        myPrinter.tonerDecrease();
+        assertEquals(99, myPrinter.getToner());
     }
 }
+
